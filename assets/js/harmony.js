c657f650a773a90c69e174abe7f98da17d39f72d
Vahideh-Asemi <vahidehasemi@gmail.com>
2023-05-09 11:24:05 +0430

    meta tag, flip timer and some scripts added

--- assets/js/harmony.js
+++ assets/js/harmony.js
@@ -193,3 +193,42 @@ $(".callToAction").hover(
 if (window.matchMedia("(max-width: 768px)").matches) {
   console.log("mobile scripts");
 }
+
+/*===================================== timer =====================================*/
+// The data/time we want to countdown to
+var countDownDate = new Date("Jun 21, 2023 00:00:00").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function () {
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+
+  // Calculating the days, hours, minutes and seconds left
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  // Result is output to the specific element
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("mins").textContent = minutes;
+  document.getElementById("secs").textContent = seconds;
+
+  // Display the message when countdown is over
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("days").textContent = "";
+    document.getElementById("hours").textContent = "";
+    document.getElementById("mins").textContent = "";
+    document.getElementById("secs").textContent = "";
+  }
+}, 1000);
+
+$(window).on("load", function () {
+  console.log("all content (e.g. images) has been loaded.");
+});
+
+$(document).ready(function () {
+  console.log("HTML document has been loaded");
+});
