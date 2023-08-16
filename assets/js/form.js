a1c028b0637c9b9dc8ef7440c598150dfd8ece2b
unknown <vahidehasemi@gmail.com>
2023-08-16 10:06:24 +0330

    update j-v2s

--- assets/js/form.js
+++ assets/js/form.js
@@ -246,3 +246,17 @@ function countdown() {
     if (minutes == 0 && seconds == 0) clearInterval(interval);
   }, 1000);
 }
+
+$("#phoneNumber").on("change keyup paste input", function (event) {
+  $("#phoneNumber").val(translateToEng(this.value));
+});
+
+function translateToEng(text) {
+  var pn = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  var en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  for (var i = 0; i < 10; i++) {
+    var regex_fa = new RegExp(pn[i], "g");
+    text = text.replace(regex_fa, en[i]);
+  }
+  return text;
+}
